511363da72e61df3e296ab93ebddb9dea62479ee
Dawson Richey <dawson.richey@ttu.edu>
2020-07-22 05:25:35 -0500

    something is really wrong with the classes I may need to revert.

--- fake_website.py
+++ fake_website.py
@@ -1,102 +1,26 @@
-# Dawson Richey
-# This will serve as the main app file for the project
-import os
-from datetime import datetime, date
+# Author Dawson Richey
+"""A class that can be a user"""
 
-time = datetime.time()
-date = date.date()
-# Time abbreviation, hour min, and second
-current_time = time.strftime("%H:%M:%S")
-# Date abbreviation, day and year
-current_date = date.strftime("%b-%d-%Y")
+class User:
 
-full_datetime = print("Time and Date =", current_time, current_date)
+    def __init__(self, first_name, last_name, username, password, current_time):
+        """Initialize attributes to describe a user."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+        self.current_time = current_time
 
+    def get_full_name(self):
+        """Return full name."""
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.title()
 
-# FUNCTIONS
-def program_title_bar():
-    # Clears the terminal screen, and displays a title bar.
-    os.system('clear')
-    print("****************************************************************")
-    print("***      You are about to enter another dimension.           ***")
-    print("***   A dimension not only of sight and sound, but of mind.  ***")
-    print("***       A journey into a wondrous land of imagination.     ***")
-    print("****************************************************************")
-
-
-def store_title_bar():
-    # Clears the terminal screen, and displays a Store title bar.
-    os.system('clear')
-    print("****************************************************************")
-    print("***                   Oleanders Wand Shop                    ***")
-    print("****************************************************************")
+    def new_user(self, first_name, last_name, username, password):
+        """ new user input """
+        self.first_name = input(f'Username: {first_name}')
+        self.last_name = input(f'Password: {last_name}')
+        self.username = input(f'Username: {username}')
+        self.password = input(f'Password: {password}')
 
 
-"""A class that can be used to represent a car."""
-
-
-class Car:
-    """A simple attempt to represent a car."""
-
-    def __init__(self, make, model, year):
-        """Initialize attributes to describe a car."""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-
-    def get_descriptive_name(self):
-        """Return a neatly formatted descriptive name."""
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
-
-    def read_odometer(self):
-        """Print a statement showing the car's mileage."""
-        print(f"This car has {self.odometer_reading} miles on it.")
-
-    def update_odometer(self, mileage):
-        """
-        Set the odometer reading to the given value.
-        Reject the change if it attempts to roll the odometer back.
-        """
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        """Add the given amount to the odometer reading."""
-        self.odometer_reading += miles
-
-
-class Battery:
-    """A simple attempt to model a battery for an electric car."""
-
-    def __init__(self, battery_size=75):
-        """Initialize the battery's attributes."""
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
-
-    def get_range(self):
-        """Print a statement about the range this battery provides."""
-        if self.battery_size == 75:
-            range = 260
-        elif self.battery_size == 100:
-            range = 315
-
-        print(f"This car can go about {range} miles on a full charge.")
-
-
-class ElectricCar(Car):
-    """Models aspects of a car, specific to electric vehicles."""
-
-    def __init__(self, make, model, year):
-        """
-        Initialize attributes of the parent class.
-        Then initialize attributes specific to an electric car.
-        """
-        super().__init__(make, model, year)
-        self.battery = Battery()
--- user.py
+++ user.py
@@ -1,27 +1,72 @@
-# Author Dawson Richey
-"""A class that can be a user"""
+# Dawson Richey
+# This will serve as the main app file for the project
+import datetime
+import os
+from datetime import time, date
+# from fake_website import User
+# from user import get_full_name
 
-class User:
+time = datetime.time()
+date = datetime.date()
+# Time abbreviation, hour min, and second
+current_time = time.strftime("%H:%M:%S")
+# Date abbreviation, day and year
+current_date = date.strftime("%b-%d-%Y")
 
-    def __init__(self, first_name, last_name, username, password, current_time):
-        """Initialize attributes to describe a user."""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.username = username
-        self.password = password
-        self.current_time = current_time
-
-    def get_full_name(self):
-        """Return full name."""
-        full_name = f"{self.first_name} {self.last_name}"
-        return full_name.title()
-
-    def new_user(self, first_name, last_name, username, password):
-        """ new user input """
-        self.first_name = input(f'Username: {first_name}')
-        self.last_name = input(f'Password: {last_name}')
-        self.username = input(f'Username: {username}')
-        self.password = input(f'Password: {password}')
+full_datetime = print("Time and Date =", current_time, current_date)
 
 
+# FUNCTIONS
+def program_title_bar():
+    # Clears the terminal screen, and displays a title bar.
+    os.system('clear')
+    print("****************************************************************")
+    print("***      You are about to enter another dimension.           ***")
+    print("***   A dimension not only of sight and sound, but of mind.  ***")
+    print("***       A journey into a wondrous land of imagination.     ***")
+    print("****************************************************************")
 
+
+def menu_options():
+    # this is the menu of options to keep the app running
+    print("\nPlease select a number from the following: ")
+    print("0 = Sign-up")
+    print("1 = Sign-in")
+    print("2 = Log-out")
+    print("(enter 'q' at any time to quit)")
+
+
+print(full_datetime)
+
+program_title_bar()
+menu_selection = input(menu_options())
+# This is an infinite loop!
+
+while menu_selection != 'q':
+    num_selected = int(input("select one number from the following list"))
+    user.new_user()
+
+
+
+
+# class User:
+#
+#     def __init__(self, first_name, last_name, username, password, current_time):
+#         """Initialize attributes to describe a user."""
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.username = username
+#         self.password = password
+#         self.current_time = current_time
+#
+#     def get_full_name(self):
+#         """Return full name."""
+#         full_name = f"{self.first_name} {self.last_name}"
+#         return full_name.title()
+#
+#     def new_user(self, first_name, last_name, username, password):
+#         """ new user input """
+#         self.first_name = input(f'Username: {first_name}')
+#         self.last_name = input(f'Password: {last_name}')
+#         self.username = input(f'Username: {username}')
+#         self.password = input(f'Password: {password}')
